3e9271ed22fd196a45d92f5ee8e8298cd736858f
Pranav Unnikrishnan <pranav.unnikrishnan01@gmail.com>
2025-01-27 15:53:14 -0700

    fix currency issue again

--- player.py
+++ player.py
@@ -94,7 +94,6 @@ def GetPlayerStats(player_id):
         # Compute the average market value for each year
         for year in range(2020, 2026):
             if player[f'{year}MV']:  # Check if there are any market values for that year
-                print(player[f'{year}MV'])
                 player[f'{year}AvgMV'] = sum(player[f'{year}MV']) / len(player[f'{year}MV'])
             else:
                 player[f'{year}AvgMV'] = 0  # If no values, assign 0
